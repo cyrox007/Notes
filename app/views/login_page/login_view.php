10e23587d4c1dd32505090045e0a0b5ebbc61cb5
cyrox007 <cyrox007@gmail.com>
2022-07-07 17:07:41 +0300

    minor fix

--- app/views/login_page/login_view.php
+++ app/views/login_page/login_view.php
@@ -1,4 +1,4 @@
-<main class="login-page">
+<div class="login-page">
     <div class="login-page__box">
         <div class="login-page__title">
             <span>
@@ -6,7 +6,6 @@
             </span>
         </div>
         <div class="login-page__card">
-            <!-- <p class="login-page_msg">Войдите в систему, чтобы начать сеанс</p> -->
             <form action="" method="post">
                 <input type="text" name="login" id="login" placeholder="Логин" required>
                 <input type="password" name="password" id="password" placeholder="Пароль" required>
@@ -14,4 +13,5 @@
             </form>
         </div>
     </div>
-</main>
+    
+</div>
