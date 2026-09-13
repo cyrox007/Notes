1694a0b354d68a74bd3f810c7ab29f77348c5b5f
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-13 13:05:12 +0300

    messenger: align membership model with v2 schema

--- app/models/UserToDialogsModel.php
+++ app/models/UserToDialogsModel.php
@@ -1,16 +1,23 @@
 <?php
+
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Core\ORM;
 
-class UserToDialogsModel extends ORM {
+class UserToDialogsModel extends ORM
+{
     protected ?string $_tablename = 'user_to_dialogs';
 
     public int $id = 0;
     public int $dialog_id = 0;
     public int $user_id = 0;
-
-    /* public function __construct() {
-        $this->_tablename = "user_to_dialogs";
-    } */
+    public string $role = 'member';
+    public string $joined_at = '';
+    public ?int $last_read_message_id = null;
+    public int $is_deleted = 0;
+    public ?string $archived_at = null;
+    public ?string $muted_until = null;
+    public ?string $pinned_at = null;
 }
