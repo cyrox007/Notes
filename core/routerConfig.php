935b7d2a25cee6dbcdd5fba9a4e6f0a91f41d275
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-14 09:32:33 +0300

    File manager: route deletes through safe lifecycle service

--- core/routerConfig.php
+++ core/routerConfig.php
@@ -10,6 +10,7 @@ use App\Controllers\NoteShareController;
 use App\Controllers\TaskController;
 use App\Controllers\ProfileController;
 use App\Controllers\FileController;
+use App\Controllers\FileDeleteController;
 use App\Controllers\Admin\AdminController;
 use App\Controllers\Admin\SettingsController;
 use App\Controllers\MessagerController;
@@ -76,7 +77,7 @@ $router->group('/files')
     ->add('GET', '/folder/{int:folderId}/', [FileController::class, 'folder'], [LoginRequared::class], 'files_folder')
     ->add('POST', '/create-folder/', [FileController::class, 'createFolder'], [LoginRequared::class], 'files_create_folder')
     ->add('POST', '/upload/', [FileController::class, 'uploadFile'], [LoginRequared::class, UploadRateLimit::class, StorageQuotaLimit::class], 'files_upload')
-    ->add('POST', '/delete/', [FileController::class, 'delete'], [LoginRequared::class], 'files_delete')
+    ->add('POST', '/delete/', [FileDeleteController::class, 'delete'], [LoginRequared::class], 'files_delete')
     ->add('POST', '/rename/', [FileController::class, 'rename'], [LoginRequared::class], 'files_rename')
     ->add('GET', '/get/{int:fileId}/', [FileController::class, 'getFile'], [LoginRequared::class], 'files_get')
     ->endGroup();
