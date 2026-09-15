add467ab824b696f11ac219507b0e0a94fa9961e
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-16 00:11:08 +0300

    Register global HTTP license mutation guard

--- index.php
+++ index.php
@@ -72,4 +72,5 @@ try {
 }
 
 require_once SITEPATH . '/core/Router.php';
+\Core\Router::getInstance()->addGlobalMiddleware(\App\Middlewares\EnforceLicenseMutation::class);
 require_once SITEPATH . '/core/routerConfig.php';
