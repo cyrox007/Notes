e1650a3130311b3b42fb8bac9831dfe7ec9845a9
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-15 11:53:34 +0300

    Remove Tasks routes from legacy central router

--- core/routerConfig.php
+++ core/routerConfig.php
@@ -7,7 +7,6 @@ use App\Controllers\AuthController;
 use App\Controllers\NoteController;
 use App\Controllers\NoteAttachmentController;
 use App\Controllers\NoteShareController;
-use App\Controllers\TaskController;
 use App\Controllers\ProfileController;
 use App\Controllers\PublicProfileController;
 use App\Controllers\FileController;
@@ -53,19 +52,6 @@ $router->group('/notes')
     ->add('POST', '/unshare/{str:uid}', [NoteShareController::class, 'unshare'], [LoginRequared::class], 'note_unshare')
     ->add('GET', '/shared/{str:token}', [NoteShareController::class, 'view'], [], 'note_shared_view')
     ->add('GET', '/shared/{str:token}/attachment/{str:fileUid}', [NoteAttachmentController::class, 'sharedDownload'], [], 'note_shared_attachment')
-    ->endGroup();
-
-$router->group('/tasks')
-    ->add('GET', '/', [TaskController::class, 'index'], [LoginRequared::class], 'tasks')
-    ->add('POST', '/', [TaskController::class, 'create'], [LoginRequared::class], 'task_create')
-    ->add('POST', '/{str:uid}/update', [TaskController::class, 'update'], [LoginRequared::class], 'update_task')
-    ->add('POST', '/{str:uid}/delete', [TaskController::class, 'delete'], [LoginRequared::class], 'delete_task')
-    ->add('POST', '/{str:taskUid}/subtask', [TaskController::class, 'addSubtask'], [LoginRequared::class], 'add_subtask')
-    ->add('POST', '/subtask/{int:subtaskId}/toggle', [TaskController::class, 'toggleSubtask'], [LoginRequared::class], 'toggle_subtask')
-    ->add('POST', '/subtask/{int:subtaskId}/delete', [TaskController::class, 'deleteSubtask'], [LoginRequared::class], 'delete_subtask')
-    ->add('POST', '/category', [TaskController::class, 'createCategory'], [LoginRequared::class], 'create_category')
-    ->add('POST', '/{str:taskUid}/category/{int:categoryId}', [TaskController::class, 'attachCategory'], [LoginRequared::class], 'attach_category')
-    ->add('DELETE', '/{str:taskUid}/category/{int:categoryId}', [TaskController::class, 'detachCategory'], [LoginRequared::class], 'detach_category')
     ->endGroup();
 
 $router->group('/profile')
