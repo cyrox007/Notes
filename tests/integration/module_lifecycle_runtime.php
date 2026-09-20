0ad422f68d01f891eaa79ec7af52014276202398
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-20 10:51:00 +0300

    Keep lifecycle fixtures compatible with 1.0 core

--- tests/integration/module_lifecycle_runtime.php
+++ tests/integration/module_lifecycle_runtime.php
@@ -45,7 +45,7 @@ function lifecycleFixture(
         'id' => $id,
         'name' => ucfirst($id),
         'version' => '0.14.0-beta.1',
-        'core' => $core !== [] ? $core : ['min' => '0.13.0-alpha', 'max_exclusive' => '0.15.0'],
+        'core' => $core !== [] ? $core : ['min' => '0.13.0-alpha', 'max_exclusive' => '2.0.0'],
         'dependencies' => $dependencies,
         'capabilities' => ['fixture.' . $id],
         'package' => ['bundled' => $bundled, 'default_enabled' => $defaultEnabled],
