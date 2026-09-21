3dd27e61f1fdfc2c09c16d235da1a7db7cb300d4
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-21 11:41:09 +0300

    Normalize CRLF in hosting package source contract

--- tests/integration/hosting_package_source_contract.php
+++ tests/integration/hosting_package_source_contract.php
@@ -16,6 +16,7 @@ function hostingPackageAssert(bool $condition, string $message): void
 hostingPackageAssert(is_file($workflowPath), 'hosting-package.yml is missing');
 $workflow = file_get_contents($workflowPath);
 hostingPackageAssert(is_string($workflow), 'hosting-package.yml cannot be read');
+$workflow = str_replace("\r\n", "\n", $workflow);
 
 $moduleSocket = 'modules/messenger/socket/NativeMessengerServer.php';
 $legacySocket = 'app/socket/NativeMessengerServer.php';
