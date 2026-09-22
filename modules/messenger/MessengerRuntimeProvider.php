e58ed5d8dd109aa70a23f797be2433dfb6d59a38
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-22 17:07:13 +0300

    Явно закрепить CSRF на HTTP-действиях Messenger

--- modules/messenger/MessengerRuntimeProvider.php
+++ modules/messenger/MessengerRuntimeProvider.php
@@ -9,6 +9,7 @@ use App\Controllers\MessengerRealtimeController;
 use App\Controllers\MessengerGroupController;
 use App\Controllers\MessengerVoiceController;
 use App\Controllers\MessengerWorkspaceController;
+use App\Middlewares\CSRFMiddleware;
 use App\Middlewares\EnforceMessengerUploadPolicy;
 use App\Middlewares\LoginRequared;
 use App\Middlewares\RequireMessengerUse;
@@ -47,7 +48,7 @@ final class MessengerRuntimeProvider implements ModuleRuntimeProvider
             ->add('GET', '/', [MessagerController::class, 'index'], [LoginRequared::class, RequireMessengerUse::class], 'messenger')
             ->add('POST', '/socket-ticket', [MessagerController::class, 'socketTicket'], [LoginRequared::class, RequireMessengerUse::class], 'messenger_socket_ticket')
             ->add('GET', '/realtime/poll', [MessengerRealtimeController::class, 'poll'], [LoginRequared::class, RequireMessengerUse::class], 'messenger_realtime_poll')
-            ->add('POST', '/realtime/action', [MessengerRealtimeController::class, 'action'], [LoginRequared::class, RequireMessengerUse::class], 'messenger_realtime_action')
+            ->add('POST', '/realtime/action', [MessengerRealtimeController::class, 'action'], [LoginRequared::class, RequireMessengerUse::class, CSRFMiddleware::class], 'messenger_realtime_action')
             ->add('POST', '/upload', [MessagerController::class, 'uploadFile'], [LoginRequared::class, RequireMessengerUse::class, UploadRateLimit::class, EnforceMessengerUploadPolicy::class], 'messenger_upload')
             ->add('POST', '/voice-upload', [MessengerVoiceController::class, 'upload'], [LoginRequared::class, RequireMessengerUse::class, UploadRateLimit::class, EnforceMessengerUploadPolicy::class], 'messenger_voice_upload')
             ->add('POST', '/workspace/note', [MessengerWorkspaceController::class, 'createNote'], [LoginRequared::class, RequireMessengerUse::class], 'messenger_workspace_note')
