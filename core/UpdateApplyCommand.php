a1990d6c4beb588c3bfe77c22fd63a298b77d632
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-25 17:44:24 +0300

    Исправить загрузку блокировки транзакции обновлятора

--- core/UpdateApplyCommand.php
+++ core/UpdateApplyCommand.php
@@ -7,6 +7,7 @@ namespace Core;
 require_once __DIR__ . '/MigrationManifest.php';
 require_once __DIR__ . '/UpdateMigrationPreflight.php';
 require_once __DIR__ . '/UpdateProcessRunner.php';
+require_once __DIR__ . '/UpdateApplyOperationLock.php';
 
 use App\Services\MaintenanceModeService;
 use mysqli;
